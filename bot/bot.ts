b76b09eb633aea171049e3f16d2b2a99887ee78c
Nikolay Mirov <densta21@gmail.com>
2023-07-23 19:42:08 +0300

    first4 commit

--- bot/bot.ts
+++ bot/bot.ts
@@ -2,7 +2,7 @@ const { Telegraf } = require('telegraf')
 const TOKEN = '6339894854:AAHEGFLfiaMFRDUBoggmbxArNSb9ycY9jFg'
 const bot = new Telegraf(TOKEN)
 
-const web_link = 'https://burger-zo3d.vercel.app/'
+const web_link = 'https://burger-xn1l-git-main-nikolaymirov.vercel.app/'
 
 bot.start((ctx: any) =>
 	ctx.reply('Welcome :)))))', {
